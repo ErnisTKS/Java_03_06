1f4adec499180f9de090da30c9238452a77c5b6c
Famy9 <ernis701@gmail.com>
2018-01-31 15:12:08 +0200

    metoduose tik ciklai

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,23 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    static String[] mas= new String[5];
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Main objektas = new Main();
+        Masyvas();
+        Rasymas();
     }
 
-    public Main(){
-        Scanner sc = new Scanner(System.in);
-        String[] mas= new String[5];
+    public static void Masyvas(){
+
         for (int i=0; i<mas.length; i++){
             System.out.println("Iveskite zodi: ");
             mas[i]=sc.nextLine();
         }
+    }
+
+    public static void Rasymas(){
         for (int i=0; i<mas.length; i++){
             System.out.println("Masyvo " + i + " zodis yra "+mas[i]);
         }
